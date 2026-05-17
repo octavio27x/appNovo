120f51fc4d7a6da249cdacd726d93f9b94121e98
octavio27x <otavioprates345@gmail.com>
2026-05-16 23:17:00 -0300

    leout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,7 @@ import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const scheme = colorScheme === 'dark' ? 'dark' : 'light';
 
   return (
     <Tabs
